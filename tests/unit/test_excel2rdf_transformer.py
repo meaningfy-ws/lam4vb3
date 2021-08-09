0e242de7e6e2a5b8e272eb9ecd7b74ff3750c4d3
costezki <costezki.eugen@gmail.com>
2021-08-09 15:04:18 +0200

    updated tests

--- tests/unit/test_excel2rdf_transformer.py
+++ tests/unit/test_excel2rdf_transformer.py
@@ -11,12 +11,10 @@ import shutil
 from lam4vb3.builder.property_builder import make_property_worksheet
 from lam4vb3.builder.lam_classes_builder import make_lam_classes_worksheet
 from lam4vb3.builder.celex_classes_builder import make_celex_classes_worksheet
-from tests import THIS_PROJECT, INPUT_EXCEL_FILE_TEST_DATA
+from tests import THIS_PROJECT, OUTPUT_FOLDER
 
-output_folder = (THIS_PROJECT / "tests" / "output").resolve()
-input_file = INPUT_EXCEL_FILE_TEST_DATA
-shutil.rmtree(output_folder, ignore_errors=True)
-output_folder.mkdir()
+shutil.rmtree(OUTPUT_FOLDER, ignore_errors=True)
+OUTPUT_FOLDER.mkdir()
 
 
 def test_transform_sample_data_properties(test_lam_properties_df, test_lam_properties_classification_df,
